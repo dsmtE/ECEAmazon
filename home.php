38c1cade26c17b31ef2f702dc5d9213470bd97a2
JulietteTn <juliette.tranluy@edu.ece.fr>
2019-04-30 18:20:45 +0200

    ajout de la page d'accueil

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php include "header.php"?>	
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"> <!--FontAwesome's CDN pour permettre l'importation du logo-->
+
+	<!-- Bootstrap's CDN  -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+	<div class="row justify-content-center" style="transform: translate(0%, 50%);">
+		<div class="align-self-center" style="width: 300px;">
+			<i alt="LogoECEAmazon" class="fab fa-artstation fa-7x" style="float:left;margin : 20px;"></i>
+
+			<h1 class="text-center" style="top:50%; transform: translate(40%, -50%);">ECEAmazon</h1>
+
+		</div>
+	</div>
+</body>
+</html>
