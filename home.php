f6b81f6c516caaf51caac803b71ce16a51999e5a
JulietteTn <juliette.tranluy@edu.ece.fr>
2019-05-01 12:02:22 +0200

    bblbl

--- home.php
+++ home.php
@@ -1,20 +1,20 @@
 <?php include "header.php"?>	
 
-<div class="row justify-content-center">
+<div class="row justify-content-center" style="margin : 1em;">
 	<div class=" col-5  align-self-center ">
 		<div class="row">
 			<div class="col-3">
 				<i alt="LogoECEAmazon" class="fab fa-artstation fa-7x" style="float:left;margin : 10px;"></i>
 			</div>
-			<div class="col-9" style="top: 1em;">
+			<div class="col-8" style="top: 1em;">
 				<div class="row">
 					<div class="col">
-						<h1 class="text-center" style="font-size:3em;font-style : bold; ">ECEAmazon</h1>
+						<h1 class="text-center" style="font-size:3em; font-style : bold; ">ECEAmazon</h1>
 					</div>
 				</div>
 				<div class="row">
 					<div class="col">
-						<h2 class="text-center" style="font-size : 1.7em; font-style: italic;">N°1 de la vente en ligne</h2>
+						<h2 class="text-center" style="font-size : 1.6em; font-style: italic;">N°1 de la vente en ligne</h2>
 					</div>
 				</div>
 			</div>
@@ -22,6 +22,12 @@
 
 	</div>
 </div>
-
+<div class="row justify-content-center" style="margin: 1em;">
+	<div class="col align-self-center">
+		<div class="row-3">
+			<h2 class="text-center" style="padding: 2em; font-style: italic; text-shadow: 2px 1px #3D3C3C;"> Best-sellers du moment</h2>
+		</div>
+	</div>
+</div>
 
 <?php include "footer.php"?>
