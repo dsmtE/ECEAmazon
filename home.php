e86972a428d44acec0b46a7103b29dc24087cd30
JulietteTn <juliette.tranluy@edu.ece.fr>
2019-05-01 13:12:05 +0200

    ajout du bloc des best-sellers

--- home.php
+++ home.php
@@ -1,18 +1,18 @@
 <?php include "header.php"?>	
 
-<div class="row justify-content-center" style="margin : 1em;">
+<div class="row justify-content-center" style="margin : 1em;"> <!--Bloc global-->
 	<div class=" col-5  align-self-center ">
-		<div class="row">
-			<div class="col-3">
-				<i alt="LogoECEAmazon" class="fab fa-artstation fa-7x" style="float:left;margin : 10px;"></i>
+		<div class="row"><!-- Sous bloc logo/titre-->
+			<div class="col-3"><!-- Sous colonne logo-->
+				<i alt="LogoECEAmazon" class="fab fa-artstation fa-7x" style="float:left; margin :10px;"></i>
 			</div>
-			<div class="col-8" style="top: 1em;">
-				<div class="row">
+			<div class="col-8" style="top: 1em;"><!-- Sous colonne titre/sous-titres-->
+				<div class="row"><!--sous bloc titre-->
 					<div class="col">
 						<h1 class="text-center" style="font-size:3em; font-style : bold; ">ECEAmazon</h1>
 					</div>
 				</div>
-				<div class="row">
+				<div class="row"><!-- Sous bloc sous titre-->
 					<div class="col">
 						<h2 class="text-center" style="font-size : 1.6em; font-style: italic;">N°1 de la vente en ligne</h2>
 					</div>
@@ -22,12 +22,57 @@
 
 	</div>
 </div>
-<div class="row justify-content-center" style="margin: 1em;">
+<div class="row justify-content-center" style="margin: 1em;"><!--bloc global-->
 	<div class="col align-self-center">
-		<div class="row-3">
-			<h2 class="text-center" style="padding: 2em; font-style: italic; text-shadow: 2px 1px #3D3C3C;"> Best-sellers du moment</h2>
+		
+		<div class="row-3"> <!-- sous bloc titre-->
+			<div class="col">
+				<h2 class="text-center" style="padding: 2em; font-style: italic; text-shadow: 2px 1px #3D3C3C;"> Best-sellers du moment</h2>
+			</div>
+		</div>
+		
+		<div class="row justify-content-center"> <!--sous bloc produits-->
+			<div class="col align-self-center" >
+				
+				<div class="col-3" style="padding : 1em;"> <!--sous bloc produit 1-->
+					<div class="row"><!--catégorie produit 1-->
+						<div class="col">
+							<h3 class="text-center" style="font-style : italic; float: left; text-shadow: 1px 1px #000000;">Littérature</h3>
+						</div>
+					</div>
+					<div class="row"> <!-- Photo produit 1-->
+						<div class="col">
+							<i class="far fa-image fa-8x"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-3" style="padding : 1em;"> <!--sous bloc produit 2-->
+				<div class="row"><!--catégorie produit 2-->
+					<div class="col-6">
+						<h3 class="text-center" style="font-style : italic; float: left; text-shadow: 1px 1px #000000;">Musique</h3>
+					</div>
+				</div>
+			</div>
+			<div class="col-3" style="padding : 1em;"> <!--sous bloc produit 3-->
+				<div class="row"><!--catégorie produit 3-->
+					<div class="col-6">
+						<h3 class="text-center" style="font-style : italic; float: left; text-shadow: 1px 1px #000000;">Mode</h3>
+					</div>
+				</div>
+			</div>
+			<div class="col-3" style="padding : 1em;"> <!--sous bloc produit 4-->
+				<div class="row"><!--catégorie produit 4-->
+					<div class="col-8">
+						<h3 class="text-center" style="font-style : italic; float: left; text-shadow: 1px 1px #000000;">Sport et loisirs</h3>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
+</div>
+</div>
+</div>
 </div>
 
 <?php include "footer.php"?>
