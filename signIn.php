f513d0ef34bed5de44fdf7a593b8e4228e79c4c8
JulietteTn <juliette.tranluy@edu.ece.fr>
2019-05-01 14:19:10 +0200

    page de connexion

---
+++ signIn.php
@@ -0,0 +1,45 @@
+<?php include "header.php"?>	
+<div class="container-fluid col">
+	<div class="row justify-content-center" style="margin : 1em;"> <!--Bloc global-->
+		<div class=" col-5  align-self-center ">
+			<div class="row"><!-- Sous bloc logo/titre-->
+				<div class="col-3"><!-- Sous colonne logo-->
+					<i alt="LogoECEAmazon" class="fab fa-artstation fa-7x" style="float:left; margin :10px;"></i>
+				</div>
+				<div class="col-8" style="top: 1em;"><!-- Sous colonne titre/sous-titres-->
+					<div class="row"><!--sous bloc titre-->
+						<div class="col">
+							<h1 class="text-center" style="font-size:3em; font-style : bold; ">ECEAmazon</h1>
+						</div>
+					</div>
+					<div class="row"><!-- Sous bloc sous titre-->
+						<div class="col">
+							<h2 class="text-center" style="font-size : 1.6em; font-style: italic;">N°1 de la vente en ligne</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="row justify-content-center" style="padding : 15% 0%">
+		<div class="col-sm-8">
+			<form>
+				<div class="form-group row">
+					<label for="inputEmail3" class="col-sm-2 col-form-label">Adresse mail :</label>
+					<div class="col-sm-10">
+						<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+					</div>
+				</div>
+				<div class="form-group row">
+					<label for="inputPassword3" class="col-sm-2 col-form-label">Mot de passe : </label>
+					<div class="col-sm-10">
+						<input type="password" class="form-control" id="inputPassword3" placeholder="Mot de passe">
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+<?php include "footer.php"?>
