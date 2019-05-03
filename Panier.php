cb94ac016f6727a39707014ecf219d452cd17812
cyprien-gasset <cyprien587@gmail.com>
2019-05-03 12:37:01 +0200

    Modification panier

--- Panier.php
+++ Panier.php
@@ -8,7 +8,6 @@ include "footer.php";
 
 
 
-
   <form style="padding: 40px;">
     <div class="row"> 
 
@@ -27,78 +26,50 @@ include "footer.php";
 
         <div class="row">
 
-<h1 class="text-left mt-2 mb-5">Mes Produits</h1>
+          <h1 class="text-left mt-2 mb-5">Mes Produits</h1>
           <div style=" overflow-y: scroll; " class="col-sm-12 rounded px-5">
 
+
+
+            <div class="row mb-3" style="border-style: solid; border-width: 0.1em; border-color: #ddd;">
+              <div class="col-sm-4 mt-3">
+                <img src="..." alt="..." class="img-thumbnail">
+                <p class="text-left" id="quantite">Quantité : </p>
+                <p class="text-left" id="vendeur">Vendeur</p>
+
+              </div>
+              <div class="col-sm-8 mt-3">
+                <p class="text-left" id="nomproduit">Nom du produit</p>
+                <textarea class="form-control" id="description" rows="3"></textarea>
+                <p class="text-right" id="prix">Prix</p>
+                <button type="submit" class="btn btn-primary float-right mb-3">Panier</button>
+              </div>
+            </div>
+
+            <div class="row mb-3" style="border-style: solid; border-width: 0.1em; border-color: #ddd;">
+              <div class="col-sm-4 mt-3">
+                <img src="..." alt="..." class="img-thumbnail">
+                <p class="text-left" id="quantite">Quantité : </p>
+                <p class="text-left" id="vendeur">Vendeur</p>
+
+              </div>
+              <div class="col-sm-8 mt-3">
+                <p class="text-left" id="nomproduit">Nom du produit</p>
+                <textarea class="form-control" id="description" rows="3"></textarea>
+                <p class="text-right" id="prix">Prix</p>
+                <button type="submit" class="btn btn-primary float-right mb-3">Panier</button>
+              </div>
+            </div>
+
             
-
-            <div class="row" style="border-style: solid; border-width: 0.1em; border-color: #ddd;">
-              <div class="col-sm-4">
-                <img src="..." alt="..." class="img-thumbnail">
-                <p class="text-left" id="quantite">Quantité : </p>
-                <p class="text-left" id="vendeur">Vendeur</p>
-
-              </div>
-              <div class="col-sm-8">
-                <p class="text-left" id="nomproduit">Nom du produit</p>
-                <textarea class="form-control" id="description" rows="3"></textarea>
-                <p class="text-right" id="prix">Prix</p>
-                <button type="submit" class="btn btn-primary float-right mb-3">Panier</button>
-              </div>
-            </div>
-
-            <div class="row" style="border-style: solid; border-width: 0.1em; border-color: #ddd;">
-              <div class="col-sm-4">
-                <img src="..." alt="..." class="img-thumbnail">
-                <p class="text-left" id="quantite">Quantité : </p>
-                <p class="text-left" id="vendeur">Vendeur</p>
-
-              </div>
-              <div class="col-sm-8">
-                <p class="text-left" id="nomproduit">Nom du produit</p>
-                <textarea class="form-control" id="description" rows="3"></textarea>
-                <p class="text-right" id="prix">Prix</p>
-                <button type="submit" class="btn btn-primary float-right mb-3">Panier</button>
-              </div>
-            </div>
-
-            <div class="row" style="border-style: solid; border-width: 0.1em; border-color: #ddd;">
-              <div class="col-sm-4">
-                <img src="..." alt="..." class="img-thumbnail">
-                <p class="text-left" id="quantite">Quantité : </p>
-                <p class="text-left" id="vendeur">Vendeur</p>
-
-              </div>
-              <div class="col-sm-8">
-                <p class="text-left" id="nomproduit">Nom du produit</p>
-                <textarea class="form-control" id="description" rows="3"></textarea>
-                <p class="text-right" id="prix">Prix</p>
-                <button type="submit" class="btn btn-primary float-right mb-3">Panier</button>
-              </div>
-            </div>
-
-            <div class="row" style="border-style: solid; border-width: 0.1em; border-color: #ddd;">
-              <div class="col-sm-4">
-                <img src="..." alt="..." class="img-thumbnail">
-                <p class="text-left" id="quantite">Quantité : </p>
-                <p class="text-left" id="vendeur">Vendeur</p>
-
-              </div>
-              <div class="col-sm-8">
-                <p class="text-left" id="nomproduit">Nom du produit</p>
-                <textarea class="form-control" id="description" rows="3"></textarea>
-                <p class="text-right" id="prix">Prix</p>
-                <button type="submit" class="btn btn-primary float-right mb-3">Panier</button>
-              </div>
-            </div>
-
           </div>
 
-          <div class="col-sm-8 mt-3">
+          <div class="col-sm-12 mt-3">
             <div class="dropdown-divider"></div>
-            <p class="text-left" id="nomproduit">Total</p>
+            <p class="text-left" id="nomproduit">TOTAL</p>
             <button type="submit" class="btn btn-primary float-right mb-3">Passer la commande</button>
           </div>
+
 
         </div>
       </div>
@@ -110,4 +81,3 @@ include "footer.php";
 
   </form>
 
-</html>
